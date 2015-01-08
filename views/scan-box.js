1c8987c045482229f3ada621f6bae1a158bb940a
klaemo <klaemo@fastmail.fm>
2015-01-08 21:08:17 +0100

    add scan-box form submission

--- views/scan-box.js
+++ views/scan-box.js
@@ -2,22 +2,37 @@
 var View = require('ampersand-view')
 var Popover = require('popover')
 
+var app = require('../app')
 var template = require('../templates/scan-box.jade')
 
 module.exports = View.extend({
   template: template,
 
-  events: {},
+  events: {
+    'submit form': 'onFormSubmit'
+  },
 
   initialize: function(opts) {
     this.triggerEl = opts.triggerEl
   },
 
+  onFormSubmit: function(event) {
+    event.preventDefault()
+    var code = this.query('input[name=code]').value
+    var location = app.locations.get(code)
+
+    if (location) {
+      app.navigate('/code/' + code)
+      // triggers .remove()
+      app.state.scannerVisible = false
+    } else {
+      this.query('.info').textContent = 'Nicht gefunden.'
+    }
+  },
+
   remove: function() {
-    var self = this
-    // this.popover.el.classList.add('fadeOut')
-    View.prototype.remove.call(self)
-    self.popover.remove()
+    View.prototype.remove.call(this)
+    this.popover.remove()
   },
 
   render: function() {
